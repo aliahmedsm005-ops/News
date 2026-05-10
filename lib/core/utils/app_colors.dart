6e991be326319e1a4a88e53788b70873a0741abe
Ali Ahmed <ali.ahmed.sm005@gmail.com>
2026-05-10 12:13:33 +0300

    1

--- lib/core/utils/app_colors.dart
+++ lib/core/utils/app_colors.dart
@@ -1,4 +1,17 @@
 import "package:flutter/material.dart";
 abstract class AppColors {
-
+  static const Color splashBackgroundColors= Color(0xffE9EEFA);
+  static const Color appBackgroundColors= Color(0xffFFFFFF);
+  static const Color homePageTextTitleColors= Color(0xff6D6265) ;
+  static const Color textColors= Color(0xff231F20);
+  static const Color btnTextColors= Color(0xffFFFFFF);
+  static const Color seeMoreColors= Color(0xff2D5BD0);
+  static const Color hintTextColors= Color(0xff676767);
+  static const Color textFormFieldColors= Color(0xffF0EFF0);
+  static const Color btnColors= Color(0xff2D5BD0);
+  static const Color optionsColors= Color(0xff231F20);
+  static const Color navContainerColors= Color(0xffFCE9EECC);
+  static const Color optionsContainerColors= Color(0xffE9EEFA);
+  static const Color appBarColors= Color(0xff231F20);
+  static const Color articleBarColors= Color(0xff6D6265);
 }
